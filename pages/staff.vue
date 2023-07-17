8228572039a11102fa8750a31129e8a304f81138
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-07-17 19:13:41 +0500

    added staff member

--- pages/staff.vue
+++ pages/staff.vue
@@ -7,7 +7,7 @@
                 <div class="grid grid-cols-2 gap-12 text-white w-fit 550px:grid-cols-1">
                     <div class="bg-black p-6 rounded-lg relative group">
                         <span class="absolute top-2 rounded-sm font-semibold left-2 p-2 text-sm px-3 bg-rust">Owner</span>
-                        <img class="mb-5 rounded-md group-hover:scale-125 transition-all" width="300" height="300" src="https://cdn-longterm.mee6.xyz/plugins/welcome/images/962143010177572884/1dc9fbb2db88f57b9b0bd3c76649cca1d07eda2fc2d9026d6974bfd120f6bab5.gif">
+                        <img class="mb-5 rounded-md group-hover:scale-125 transition-all" width="200" height="200" src="https://cdn-longterm.mee6.xyz/plugins/welcome/images/962143010177572884/1dc9fbb2db88f57b9b0bd3c76649cca1d07eda2fc2d9026d6974bfd120f6bab5.gif">
                         <div class="info">
                             <h2 class="text-center text-xl mb-3">UnknownGamer</h2>
                             <div class="flex text-md items-center justify-center">
@@ -17,14 +17,27 @@
                         </div>
                     </div>
                     <div class="bg-black p-6 rounded-lg relative group">
-                        <span class="absolute top-2 rounded-sm font-semibold left-2 p-2 text-sm px-3 bg-rust">Developer</span>
-                        <img class="mb-5 rounded-md group-hover:scale-125 transition-all" width="300" height="300" src="https://avatars.akamai.steamstatic.com/5d9378ab988f5ca6842d7e6f9c71e02b23928f21_full.jpg">
+                        <span class="absolute top-2 rounded-sm font-semibold left-2 p-2 text-sm px-3 bg-rust">Owner</span>
+                        <img class="mb-5 rounded-md group-hover:scale-125 transition-all" width="200" height="200" src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/items/386180/e47273b62284f3ab2e2b9b15c89607c8c1d4dd16.gif">
                         <div class="info">
-                            <h2 class="text-center text-xl mb-3">iLoBBer</h2>
+                            <h2 class="text-center text-xl mb-3">GrimKngCobra</h2>
                             <div class="flex text-md items-center justify-center">
                                 <Icon class="mr-2" name="logos:discord-icon" />
-                                <span class="text-sm">ilobber</span>
+                                <span class="text-sm">grimkngcobra</span>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-1 gap-12 text-white w-fit m-auto 550px:grid-cols-1">
+                <div class="bg-black p-6 rounded-lg relative group">
+                    <span class="absolute top-2 rounded-sm font-semibold left-2 p-2 text-sm px-3 bg-rust">Developer</span>
+                    <img class="mb-5 rounded-md group-hover:scale-125 transition-all" width="200" height="200" src="https://avatars.akamai.steamstatic.com/5d9378ab988f5ca6842d7e6f9c71e02b23928f21_full.jpg">
+                    <div class="info">
+                        <h2 class="text-center text-xl mb-3">iLoBBer</h2>
+                        <div class="flex text-md items-center justify-center">
+                            <Icon class="mr-2" name="logos:discord-icon" />
+                            <span class="text-sm">ilobber</span>
                         </div>
                     </div>
                 </div>
